7103c4a3793ab1827034b55609d5bc209e051872
Maksim Isakov <qrlikk@gmail.com>
2024-04-30 18:55:37 +0400

    109. Aura Projectile Spell

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,8 @@
+// Copyright by Aura
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+                                           const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) {
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
