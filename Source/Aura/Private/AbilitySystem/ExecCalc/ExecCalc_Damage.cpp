21fc579bc1104bad59ac57e5727e25eb692f9d47
Maksim Isakov <qrlikk@gmail.com>
2024-05-31 17:30:25 +0400

    143. ExecCalcs - Capturing Attributes

--- Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -4,8 +4,30 @@
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 
 #include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+namespace {
+	struct AuraDamageStatics {
+		// DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+
+		FGameplayEffectAttributeCaptureDefinition ArmorDef;
+
+		AuraDamageStatics():
+			ArmorDef(UAuraAttributeSet::GetArmorAttribute(), EGameplayEffectAttributeCaptureSource::Target, false) {
+			// need to make UAuraAttributeSet::Armor public
+			// also no need to capture not snapshot attribute, because we can get them directly from ASC
+			// DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, EGameplayEffectAttributeCaptureSource::Target, false);
+		}
+	};
+
+	const AuraDamageStatics& DamageStatics() {
+		static AuraDamageStatics DStatics;
+		return DStatics;
+	}
+}
 
 UExecCalc_Damage::UExecCalc_Damage() {
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -17,4 +39,15 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	auto* Target = TargetAsc->GetAvatarActor();
 
 	const auto& Spec = ExecutionParams.GetOwningSpec();
+
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	EvaluateParameters.TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	float Armor = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, Armor);
+	++Armor;
+
+	FGameplayModifierEvaluatedData EvaluatedData(UAuraAttributeSet::GetArmorAttribute(), EGameplayModOp::Additive, Armor);
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
